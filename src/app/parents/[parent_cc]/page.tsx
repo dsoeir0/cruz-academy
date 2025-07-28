f1174ef6c75d71d3e5becc149c5ef28ae87804bd
Duarte <dsoeiro2003@gmail.com>
2025-07-28 15:27:37 +0100

    Added the parent  login layout page

--- src/app/parents/[parent_cc]/page.tsx
+++ src/app/parents/[parent_cc]/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="size-full flex justify-center content-center">
         <button>
-            Pagina inicial ainda nao contem nada
+            Tabela com todos as disciplinas do aluno e o sumario de cada licao
         </button>
     </div>
   );
--- src/app/parents/page.tsx
+++ src/app/parents/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-        
+    <div className="size-full flex justify-center content-center">
+        <button>
+            <Link href={'/parents/1'} className='p-6 bg-blue-500 content-center w-36 text-center duration-500 hover:bg-blue-800 hover:cursor-pointer text-white rounded-2xl'>Encarregado </Link>
+        </button>
     </div>
   );
 }
